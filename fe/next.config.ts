ae48532c455c4f04cd5f7b9f32623ef04965babc
LimFull <full7002@naver.com>
2025-03-23 20:20:57 +0900

    fix: pwd test

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,23 +1,33 @@
-import type {NextConfig} from "next";
+import next_pwa from "next-pwa";
 
-const isProd = process.env.NEXT_PUBLIC_MODE === 'prod';
-const prefix = isProd ? `${process.env.NEXT_PUBLIC_URL}` : ''
+const isProd = process.env.NEXT_PUBLIC_MODE === "prod";
+const prefix = isProd ? `${process.env.NEXT_PUBLIC_URL}` : "";
+const withPWA = next_pwa({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: !isProd, // dev 모드에선 꺼두기
+});
 
+const nextConfig = {
+  // ...(isProd
+  //   ? { output: "export", basePath: "/son-tracker", assetPrefix: prefix }
+  //   : {}),
+  output: "export" as "export" | undefined,
+  basePath: "/son-tracker",
+  assetPrefix: prefix,
 
-const nextConfig: NextConfig = {
-    ...(isProd ? {output: 'export', basePath: '/son-tracker', assetPrefix: prefix,} : {}),
-
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'search.pstatic.net',
-                port: '',
-                pathname: '/**',
-            }
-        ]
-    }
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https" as "https" | "http",
+        hostname: "search.pstatic.net",
+        // port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -1,39 +1,52 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {Providers} from "@/components/modules/Providers";
+import { Providers } from "@/components/modules/Providers";
 
 const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-    title: "Son Tracker",
-    description: "track matches and push notification",
+  title: "Son Tracker",
+  description: "track matches and push notification",
+  manifest: "/son-tracker/manifest.json",
 };
 
-
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" className={'h-full'}>
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-        >
-        <Providers>
-            {children}
-        </Providers>
-        </body>
-        </html>
-    );
+  return (
+    <html lang="en" className={"h-full"}>
+      <head>
+        {/* manifest 연결 */}
+        <link rel="manifest" href="/son-tracker/manifest.json" />
+
+        {/* iOS 전용 아이콘 */}
+        <link rel="apple-touch-icon" href="/son-tracker/icon512_rounded.png" />
+
+        {/* iOS PWA 설정 */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="son tracker" />
+
+        {/* PWA 색상 */}
+        <meta name="theme-color" content="#8936FF" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
