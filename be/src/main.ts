1737793ae042f56a410ee57b2a4419ced9cc6a4b
LimFull <full7002@naver.com>
2025-05-26 21:59:04 +0900

    feat: https api server

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { winstonLogger } from './utils/winston';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/sontrackandpushbe.shop/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/sontrackandpushbe.shop/fullchain.pem'),
+  };
+
   const app = await NestFactory.create(AppModule, {
+    httpsOptions,
     logger: winstonLogger,
   });
   app.enableCors();
 
   try {
-    await app.listen(process.env.PORT ?? 4001);
+    await app.listen(process.env.PORT ?? 443);
   } catch (e) {
     winstonLogger.log('CRASH!!!', e);
   }
