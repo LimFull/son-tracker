b34988762c2ab00ee05279a7b59137c480ee567c
LimFull <full7002@naver.com>
2025-05-27 17:21:16 +0900

    chore: 불필요 로그 제거

--- fe/src/hooks/useFirebase.ts
+++ fe/src/hooks/useFirebase.ts
@@ -64,16 +64,6 @@ export const useFirebase = () => {
   useEffect(() => {
     const initializeFirebase = async () => {
       try {
-        console.log('Firebase 설정값:', {
-          apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-          authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-          projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-          storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-          measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-        });
-        
         console.log('Firebase 초기화 시작');
         const app = initializeApp(firebaseConfig);
         const messagingInstance = getMessaging(app);
